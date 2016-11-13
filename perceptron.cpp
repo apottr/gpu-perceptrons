174cafe9fd3e4ad8e77b75081b9dad3191911821
apotter <alexwpotter@gmail.com>
2016-11-13 05:11:17 -0500

    0511 -0400 gnight

--- perceptron.cpp
+++ perceptron.cpp
@@ -2,25 +2,25 @@
 #include <iomanip>
 using namespace std;
 
-#define ARRAY_SIZE(array) (sizeof((array))/sizeof((array[0])))
+#define ARRAY_SIZE(array) (sizeof(array)/sizeof(float))
 
 float dot(float *a, float *b, size_t n);
-float perceptron(float *trainingdata, float *weights, int n);
+float perceptron(float *trainingdata, float *weights, int generations, int arr_length);
 int render(float input);
 
 int main(){
 	float test_one[] = {2.0,2.0,2.0,2.0};
 	float test_two[] = {1.0,1.0,1.0,1.0};
-	render(dot(test_one,test_two,ARRAY_SIZE(test_one)));
+	render(perceptron(test_one,test_two,1,ARRAY_SIZE(test_one)));
 	return 0;
 }
 
 int render(float input){
-	cout << std::fixed << std::setprecision(1) << input << "\n";
+	cout << std::fixed << std::setprecision(1) << static_cast<float>(input) << "\n";
 	return 0;
 }
 
-float dot(float *a, float *b, size_t n){
+float dot(float *a, float *b, int n){
 	float out = 0.0;
 	for(int i = 0; i<n; i++){
 		out += a[i] * b[i];
@@ -28,9 +28,8 @@ float dot(float *a, float *b, size_t n){
 	return out;
 }
 
-float perceptron(float *trainingdata, float *weights, int generations){
+float perceptron(float *trainingdata, float *weights, int generations, int arr_length){
 	//const int i = threadIdx.x;
-	size_t arr_length = ARRAY_SIZE(trainingdata);
 	float output = 0.0;
 	for(int j = 0; j < generations; j++){
 		float result = dot(trainingdata,weights,arr_length);
