b0d43e47e822016f16bb9f5151319da9c2b010db
apotter <alexwpotter@gmail.com>
2016-11-13 20:51:31 -0500

    seven errors but i gotta go

--- perceptron.cpp
+++ perceptron.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
 #include <iomanip>
-using namespace std;
+#include <vector>
 
-#define ARRAY_SIZE(array) (sizeof(array)/sizeof(float))
+#define ARRAY_SIZE(array) (sizeof(array)/sizeof(array[0]))
+#define UNIT_STEP(input) ((input < 0.0) ? (0.0) : (1.0))
 
-float dot(float *a, float *b, size_t n);
-float perceptron(float *trainingdata, float *weights, int generations, int arr_length);
-int render(float input);
-
-int main(){
-	float test_one[] = {2.0,2.0,2.0,2.0};
-	float test_two[] = {1.0,1.0,1.0,1.0};
-	render(perceptron(test_one,test_two,1,ARRAY_SIZE(test_one)));
-	return 0;
-}
-
-int render(float input){
-	cout << std::fixed << std::setprecision(1) << static_cast<float>(input) << "\n";
+int render(float *input,int length=0){
+	if(length != 0){
+		for(var i = 0; i<length; i++){
+			std::cout << std::fixed << std::setprecision(1) << static_cast<float>(input);
+		}
+		std::cout << "\n";
+	}else{
+		std::cout << std::fixed << std::setprecision(1) << static_cast<float>(input) << "\n";
+	}
 	return 0;
 }
 
@@ -28,12 +25,56 @@ float dot(float *a, float *b, int n){
 	return out;
 }
 
-float perceptron(float *trainingdata, float *weights, int generations, int arr_length){
+float array_adjust(float *weights, int length, float *adjustment, int adj_length=0){
+	if(adj_length != 0){
+		for(int j = 0; j < adj_length; j++){
+			for(int i = 0; i < length; i++){
+				weights[i] += adjustment[j];
+			}
+		}
+}
+
+float perceptron(float (*trainingdata)[2][3], float *weights, int generations, int input_length, int training_length){
 	//const int i = threadIdx.x;
 	float output = 0.0;
+	float eta = 0.2;
+
 	for(int j = 0; j < generations; j++){
-		float result = dot(trainingdata,weights,arr_length);
+		float result = dot(trainingdata[0][0],weights,input_length);
+		render(result);
+		if(UNIT_STEP(result) != trainingdata[0][1]){
+			array_adjust(weights,3,(() * -1.0))
+		}else{
+			array_adjust(weights,3,())
+		}
 		output += result;
 	}
 	return output;
+}
+
+float test_perceptron(float *weights, float **testingdata, int length, int w_length){
+	for(int i = 0; i<length; i++){
+		float result = dot(testingdata[i],weights,w_length);
+		render(testing_data[i],w_length);
+		render(result);
+	}
+}
+
+int main(){
+	float training_data[4][2][3] = {
+		{{1.0,0.0,1.0},0.0},
+		{{1.0,1.0,1.0},1.0},
+		{{0.0,1.0,1.0},0.0},
+		{{0.0,0.0,1.0},0.0}
+	};
+	float testing_data[][] = {
+		{1.0,0.0,1.0},
+		{0.0,0.0,1.0},
+		{1.0,1.0,1.0},
+		{0.0,1.0,1.0}
+	};
+	float weights[] = {0.0,0.0,0.0};
+	perceptron(training_data,weights,1,ARRAY_SIZE(weights),ARRAY_SIZE(training_data));
+	test_perceptron(weights,testing_data,4);
+	return 0;
 }
