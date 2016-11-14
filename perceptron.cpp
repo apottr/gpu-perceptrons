787949364865860bf5c756b9b3ceebbee26b6ac9
apotter <alexwpotter@gmail.com>
2016-11-14 03:43:38 -0500

    training works\!

--- perceptron.cpp
+++ perceptron.cpp
@@ -1,80 +1,96 @@
 #include <iostream>
 #include <iomanip>
-#include <vector>
+#include <cstdlib>
 
 #define ARRAY_SIZE(array) (sizeof(array)/sizeof(array[0]))
 #define UNIT_STEP(input) ((input < 0.0) ? (0.0) : (1.0))
 
-int render(float *input,int length=0){
-	if(length != 0){
-		for(var i = 0; i<length; i++){
-			std::cout << std::fixed << std::setprecision(1) << static_cast<float>(input);
-		}
-		std::cout << "\n";
-	}else{
-		std::cout << std::fixed << std::setprecision(1) << static_cast<float>(input) << "\n";
+int render_array(double *input,int length){
+	for(int i = 0; i<length; i++){
+		std::cout << std::fixed << std::setprecision(1) << *input << ", ";
 	}
+	std::cout << "\n";
 	return 0;
 }
 
-float dot(float *a, float *b, int n){
-	float out = 0.0;
+int render_double(double input){
+	std::cout << std::fixed << std::setprecision(1) << input << "\n";
+	return 0;
+}
+
+double dot(double *a, double *b, int n){
+	double out = 0.0;
 	for(int i = 0; i<n; i++){
 		out += a[i] * b[i];
 	}
 	return out;
 }
 
-float array_adjust(float *weights, int length, float *adjustment, int adj_length=0){
-	if(adj_length != 0){
-		for(int j = 0; j < adj_length; j++){
-			for(int i = 0; i < length; i++){
-				weights[i] += adjustment[j];
-			}
+double array_adjust(double *weights, int length, double *adjustment, double eta, int toggle_sign=0){
+	for(int i = 0; i < length; i++){
+		if(toggle_sign == 0){
+			weights[i] += adjustment[i] * eta;
+		}else{
+			weights[i] -= adjustment[i] * eta;
 		}
+	}
+	return 0.0;
 }
 
-float perceptron(float (*trainingdata)[2][3], float *weights, int generations, int input_length, int training_length){
+double double_adjust(double *weights, int length, double adjustment, int toggle_sign=0){
+	for(int i = 0; i < length; i++){
+		if(toggle_sign == 0){
+			weights[i] += adjustment;
+		}else if(toggle_sign == 1){
+			weights[i] -= adjustment;
+		}
+	}
+	return 0.0;
+}
+
+double perceptron(double (*trainingdata)[2][3], double *weights, int generations, int input_length, int training_length){
 	//const int i = threadIdx.x;
-	float output = 0.0;
-	float eta = 0.2;
+	double eta = 0.2;
 
 	for(int j = 0; j < generations; j++){
-		float result = dot(trainingdata[0][0],weights,input_length);
-		render(result);
-		if(UNIT_STEP(result) != trainingdata[0][1]){
-			array_adjust(weights,3,(() * -1.0))
+		int term = 0 + (std::rand() % (int)(training_length - 1));
+		double result = dot(trainingdata[term][0],weights,input_length);
+
+		if(UNIT_STEP(result) != *trainingdata[term][1]){
+			array_adjust(weights,3,trainingdata[term][0],eta,1); //second step: training data
 		}else{
-			array_adjust(weights,3,())
+			array_adjust(weights,3,trainingdata[term][0],eta); //second step: training data
 		}
-		output += result;
 	}
-	return output;
+	return 0.0;
 }
 
-float test_perceptron(float *weights, float **testingdata, int length, int w_length){
+double test_perceptron(double *weights, double (testingdata)[4][3], int length, int w_length){
+	render_array(testingdata[0],length);
 	for(int i = 0; i<length; i++){
-		float result = dot(testingdata[i],weights,w_length);
-		render(testing_data[i],w_length);
-		render(result);
+		double result = dot(testingdata[i],weights,w_length);
+		render_array(testingdata[i],w_length);
+		render_double(result);
+		render_double(UNIT_STEP(result));
 	}
+	return 0.0;
 }
 
 int main(){
-	float training_data[4][2][3] = {
+	double training_data[4][2][3] = {
 		{{1.0,0.0,1.0},0.0},
 		{{1.0,1.0,1.0},1.0},
 		{{0.0,1.0,1.0},0.0},
 		{{0.0,0.0,1.0},0.0}
 	};
-	float testing_data[][] = {
+	double testing_data[4][3] = {
 		{1.0,0.0,1.0},
 		{0.0,0.0,1.0},
 		{1.0,1.0,1.0},
 		{0.0,1.0,1.0}
 	};
-	float weights[] = {0.0,0.0,0.0};
-	perceptron(training_data,weights,1,ARRAY_SIZE(weights),ARRAY_SIZE(training_data));
-	test_perceptron(weights,testing_data,4);
+	double weights[] = {0.0,0.0,0.0};
+	perceptron(training_data,weights,100,ARRAY_SIZE(weights),ARRAY_SIZE(training_data));
+	test_perceptron(weights,testing_data,1,3);
 	return 0;
 }
